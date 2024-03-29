f62a51862633510ba93ba299881a8341a4a92ac8
user <abeliavtsev@techmategroup.com>
2024-03-29 09:47:11 +0100

    fix calculation

--- src/main/java/org/example/NumberInPowerCalculator.java
+++ src/main/java/org/example/NumberInPowerCalculator.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class NumberInPowerCalculator {
     public static int calculatePower(int num, int power) {
-        int result = 0;
+        int result = 1;
         for (int i = 0; i < power; i++) {
-            result += num * num;
+            result *= num;
         }
         return result;
     }
